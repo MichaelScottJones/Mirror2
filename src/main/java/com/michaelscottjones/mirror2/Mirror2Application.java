55af712e3827f83dc6883abf2165833d7683a80f
MichaelScottJones <michaelscottjones0@gmail.com>
2018-12-13 12:31:42 -0500

    Initial commit

---
+++ src/main/java/com/michaelscottjones/mirror2/Mirror2Application.java
@@ -0,0 +1,14 @@
+package com.michaelscottjones.mirror2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mirror2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mirror2Application.class, args);
+	}
+
+}
+
